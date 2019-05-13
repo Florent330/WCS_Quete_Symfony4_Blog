0456a695becf10baefb6216dd3bf1d0b171998e7
Duval Florent <Florent_Duval_student2019@wilder.school>
2019-05-13 19:35:43 +0200

    quete 7

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,26 +14,90 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="Blog_index")
+     * Show all row from article's entity
+     *
+     * @Route("/", name="index")
+     * @return Response A response instance
      */
 
-    public function index()
+    public function index (): Response
     {
+        $articles = $this->getDoctrine()->getRepository(Article::class)
+            ->findAll();
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
         return $this->render('Blog/index.html.twig', [
-            'owner' => 'Thomas',
+            'articles' => $articles,
         ]);
     }
 
     /**
-     * @Route("/blog/show/{slug<[a-z-0-9-]+>?article-sans-titre}", name="Blog_show")
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     * @return Response A response instance
      */
 
-    public function show( $slug)
+    public function show ( ?string $slug ): Response
     {
-        $slugArray = explode("-", $slug);
-        $title = ucwords(implode(" ", $slugArray));
-        return $this->render('Blog/show.html.twig', [
-            'title' => $title,
-        ]);
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'Blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/blog/category/{category}", name="show_category")
+     ** @return Response A response instance
+     **/
+
+    public function showByCategory ( string $category )
+    {
+
+        $categoryName = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $category]);
+
+        $articles = $this->getDoctrine()
+            ->getRepository((Article::class))
+            ->findBy(['category' => $categoryName]);
+        return $this->render(
+            'Blog/category.html.twig',
+            [
+                'category' => $categoryName,
+                'articles' => $articles,
+            ]
+        );
+
     }
 }
