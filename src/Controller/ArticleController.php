009459a3adf7cbb6f5365ad5943a9e41ba626a15
Duval Florent <Florent_Duval_student2019@wilder.school>
2019-06-13 11:01:19 +0200

    quete 16

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
-use App\Service\Slugify;
 use App\Repository\ArticleRepository;
+use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +18,10 @@ class ArticleController extends AbstractController
 {
     /**
      * @Route("/", name="article_index", methods={"GET"})
+     * @param ArticleRepository $articleRepository
+     * @return Response
      */
-    public function index(ArticleRepository $articleRepository): Response
+    public function index ( ArticleRepository $articleRepository ): Response
     {
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
@@ -30,9 +32,10 @@ class ArticleController extends AbstractController
      * @Route("/new", name="article_new", methods={"GET","POST"})
      * @param Request $request
      * @param Slugify $slugify
+     * @param \Swift_Mailer $mailer
      * @return Response
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new ( Request $request, Slugify $slugify, \Swift_Mailer $mailer ): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
@@ -44,6 +47,19 @@ class ArticleController extends AbstractController
             $article = $article->setTitle($article->getSlug());
             $entityManager->persist($article);
             $entityManager->flush();
+            $message = (new \Swift_Message('Un nouvel article vient d\'être publié ! '))
+                ->setFrom($this->getParameter('mailer_from'))
+                ->setTo($this->getParameter('mailer_from'))
+                ->setBody(
+                    $this->renderView(
+                        'Email/notification.html.twig',
+                        [
+                            'article' => $article,
+                        ]
+                    ),
+                    'text/html'
+                );
+            $mailer->send($message);
 
             return $this->redirectToRoute('article_index');
         }
@@ -59,7 +75,7 @@ class ArticleController extends AbstractController
      * @param Article $article
      * @return Response
      */
-    public function show(Article $article): Response
+    public function show ( Article $article ): Response
     {
         return $this->render('article/show.html.twig', [
             'article' => $article,
@@ -73,7 +89,7 @@ class ArticleController extends AbstractController
      * @param Slugify $slugify
      * @return Response
      */
-    public function edit(Request $request, Article $article, Slugify $slugify): Response
+    public function edit ( Request $request, Article $article, Slugify $slugify ): Response
     {
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
@@ -96,10 +112,13 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/{id}", name="article_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Article $article
+     * @return Response
      */
-    public function delete(Request $request, Article $article): Response
+    public function delete ( Request $request, Article $article ): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($article);
             $entityManager->flush();
